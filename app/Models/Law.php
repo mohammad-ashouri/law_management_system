cc855532265946f38a3c8a272761a51bf6ee5c11
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-10-19 19:23:37 +0330

    Fix laws model configuration

--- app/Models/Law.php
+++ app/Models/Law.php
@@ -11,14 +11,21 @@ class Law extends Model
     use HasFactory,SoftDeletes;
     protected $table = "laws";
     protected $fillable = [
-        'name',
-        'status'
+        'law_code',
+        'session_code',
+        'group_id',
+        'topic_id',
+        'title',
+        'body',
+        'approval_date',
+        'issue_date',
+        'promulgation_date',
+        'keywords',
+        'files_src',
+        'adder',
     ];
     protected $hidden = [
         'adder',
-        'added_date',
-        'editor',
-        'edited_date',
         'created_at',
         'updated_at',
         'deleted_at',
