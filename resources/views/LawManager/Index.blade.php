a9dffb0e39781ebf47dc021b81eeef78fa9b0d5b
Mohammad <exploiter0253@gmail.com>
2023-12-03 08:00:46 +0330

    Make svg icon for delete or edit law

--- resources/views/LawManager/Index.blade.php
+++ resources/views/LawManager/Index.blade.php
@@ -290,16 +290,16 @@
                             <td class="px-6 py-1">{{ $law->group->name }}</td>
                             <td class="px-6 py-1">{{ $law->topic->name }}</td>
                             <td class="px-6 py-1">{{ $law->approval_date }}</td>
-                            <td class="px-6 py-1">
+                            <td class="flex px-6 py-1">
                                 <form action="/Laws/edit/{{$law->id}}" method="get">
                                     <button type="submit"
-                                            class="px-4 py-2 mr-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 LawControl">
-                                        جزئیات و ویرایش
+                                            class="px-2 py-2 mr-3 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 LawControl">
+                                        <svg width="24px" height="24px" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" fill="none"><path d="m104.175 90.97-4.252 38.384 38.383-4.252L247.923 15.427V2.497L226.78-18.646h-12.93zm98.164-96.96 31.671 31.67" class="cls-1" style="fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:12;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" transform="translate(-77.923 40.646)"/><path d="m195.656 33.271-52.882 52.882" style="fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:12;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:5;stroke-dasharray:none;stroke-opacity:1" transform="translate(-77.923 40.646)"/></svg>
                                     </button>
                                 </form>
                                 <button type="submit" data-id="{{ $law->id }}"
-                                        class="px-4 py-2 mr-3 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300 deleteLaw">
-                                    حذف
+                                        class="px-2 py-2 mr-3 mt-4 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300 deleteLaw">
+                                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 22 L 19 22 L 19 7 L 5 7 z M 8 9 L 10 9 L 10 20 L 8 20 L 8 9 z M 14 9 L 16 9 L 16 20 L 14 20 L 14 9 z"/></svg>
                                 </button>
                             </td>
                         </tr>
